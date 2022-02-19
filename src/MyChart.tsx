a2161efd6a34b3b2f6a77681d1a8a799127426da
JimmyHoffa <1002984+JimmyHoffa@users.noreply.github.com>
2022-02-18 19:18:30 -0700

    Adding zoom functionality

--- src/MyChart.tsx
+++ src/MyChart.tsx
@@ -10,10 +10,13 @@ import {
   GridComponent,
   TitleComponent,
   TooltipComponent,
+  DataZoomComponent,
+  ToolboxComponent,
   LegendComponent
 } from 'echarts/components';
 
 import 'echarts/lib/chart/line';
+// import 'echarts/lib/component/datazoom';
 import 'echarts/lib/component/title';
 import 'echarts/lib/component/tooltip';
 // Import renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
@@ -23,7 +26,7 @@ import {
 
 // Register the required components
 echarts.use(
-  [TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasRenderer, LegendComponent]
+  [TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasRenderer, LegendComponent, DataZoomComponent, ToolboxComponent]
 );
 export type ChartData = { value: number; timestamp: string }[];
 const getChartOptionsWithoutData = (tokenName: string, data: ChartData, axisTitle?: string) => {
@@ -64,6 +67,23 @@ const getChartOptionsWithoutData = (tokenName: string, data: ChartData, axisTitl
       type: "line",
       name: tokenName,
       showSymbol: false
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        throttle: 50
+      }
+    ],
+    toolbox: {
+      left: 'center',
+      itemSize: 15,
+      top: 15,
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none'
+        },
+        restore: {}
+      }
     }
   };
 }
